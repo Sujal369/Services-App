3f7b3def8017af1fd9b19b025016ebc8d6aa7084
Sujal Lothe <130586852+Sujal369@users.noreply.github.com>
2024-06-18 12:50:11 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Zero_Coders</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/@Zero_Coders" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/sujallothe" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Sujal369" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
